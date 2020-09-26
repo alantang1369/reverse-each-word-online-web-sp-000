02a2d394655ea0c7be9c37b2c53bf89545238f0b
Alan Tang <giabao_tang@yahoo.com>
2020-09-26 00:22:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  array.collect do |word|
+  reversed_array = array.collect do |word|
     word.reverse 
   end  
-  array.join(" ")
+  reversed_array.join(" ")
 end  
